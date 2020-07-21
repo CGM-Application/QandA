a58606888bcfa0b2b01425d897a41abda6d98207
usman-ashraf <musman.ashraf@yahoo.com>
2020-07-21 15:51:13 +0200

    latest changes for review

--- src/test/java/com/cgm/qanda/service/QuestionAnswerServiceImplTest.java
+++ src/test/java/com/cgm/qanda/service/QuestionAnswerServiceImplTest.java
@@ -21,7 +21,9 @@ import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ActiveProfiles("test")
@@ -51,6 +53,82 @@ public class QuestionAnswerServiceImplTest {
         return question;
     }
 
+
+    @Test
+    public void addQuestionTest() {
+        Question q = createQuestionEntity();
+        q.setQuestion("question");
+        Mockito.when(repo.save(q)).thenReturn(q);
+        Mockito.when(repo.findByQuestion("question")).thenReturn(Optional.ofNullable(q));
+        service.addQuestion("question", "answer");
+        List<String> answers = service.getAnswers("question");
+        assertNotNull(answers);
+        assertEquals("answer", answers.get(0));
+
+    }
+    
+    /**
+     * Creator: Usman Ashraf
+     * Date: 21.07.2020
+     * Description: Test to verify if the answers is correct (as provided)
+     */
+    @Test
+    public void answerTest() {
+        Question q = createQuestionEntity();
+        q.setQuestion("question");
+        Mockito.when(repo.save(q)).thenReturn(q);
+        Mockito.when(repo.findByQuestion("question")).thenReturn(Optional.ofNullable(q));
+        service.addQuestion("question", "answer1");
+        List<String> answers = service.getAnswers("question");
+        assertNotNull(answers);
+        assertNotEquals("\"" + "the answer to life, universe and everything is 42" + "\"" + " according to" + "\""
+                + "The hitchhikers guide to the Galaxy" + "\"", answers.get(0));
+
+    }
+        
+    /**
+     * Creator: Usman Ashraf
+     * Date: 21.07.2020
+     * Description: Test to verify if the answers are in the same order as provided while adding question 
+     * Comments: This test fails occasionally. The answers should ideally be in the same order as provided.  
+     */
+    @Test
+    public void answersSequenceTest() {
+        Question q = createQuestionEntity();
+        q.setQuestion("question");
+        Mockito.when(repo.save(q)).thenReturn(q);
+        Mockito.when(repo.findByQuestion("question")).thenReturn(Optional.ofNullable(q));
+        service.addQuestion("question","answer1"+ "\""+ "answer2"+"\""+"answer3");
+        List<String> answers = service.getAnswers("question");
+        assertNotNull(answers);
+        assertEquals("answer1", answers.get(0));
+        assertEquals("answer2", answers.get(1));
+        assertEquals("answer3", answers.get(2));
+
+    }
+    
+    /**
+     * Creator: Usman Ashraf
+     * Date: 21.07.2020
+     * Description: Test to verify if the answers has the same number/size as provided while adding question 
+     */
+    @Test
+    public void answersSizeTest() {
+        Question q = createQuestionEntity();
+        q.setQuestion("question");
+        Mockito.when(repo.save(q)).thenReturn(q);
+        Mockito.when(repo.findByQuestion("question")).thenReturn(Optional.ofNullable(q));
+        service.addQuestion("question","answer1"+ "\""+ "answer2"+"\""+"answer3"+"\""+"answer4"+"\""+"answer5");
+        List<String> answers = service.getAnswers("question");
+        assertNotNull(answers);
+        assertEquals(5, answers.size());
+
+    }
+    
+    /**
+     * Description: Similar to the above e.g. answersSizeTest() this test is to verify if the answers has the same number/size as according to given input
+     */
+    
     @Test
     public void testGetAnswers() {
         Question q = createQuestionEntity();
@@ -59,17 +137,101 @@ public class QuestionAnswerServiceImplTest {
         assertNotNull(answers);
         assertEquals(1, answers.size());
     }
-
+    
+    
+    /**
+     * Creator: Usman Ashraf
+     * Date: 21.07.2020
+     * Description: Test to verify if user asks a question with a question mark "?" at the end the question, if it prints the corresponding answer" 
+     * Comments: This test fails, it is common for uses to add question mark at the end of question however, our application fails to meet this requirement 
+     */
+    
     @Test
-    public void addQuestionTest() {
+    public void askingQuestionWithQuestionMarkTest() {
         Question q = createQuestionEntity();
         q.setQuestion("question");
         Mockito.when(repo.save(q)).thenReturn(q);
         Mockito.when(repo.findByQuestion("question")).thenReturn(Optional.ofNullable(q));
-        service.addQuestion("question", "answer1");
-        List<String> answers = service.getAnswers("question");
-        assertNotNull(answers);
-        assertEquals("answer1", answers.get(0));
+        service.addQuestion("question", "answer");
+        List<String> answers = service.getAnswers("question?");
+        assertEquals("answer", answers.get(0));
 
     }
+    
+    /**
+     * Creator: Usman Ashraf
+     * Date: 21.07.2020
+     * Description: Test if user provides a similar question with updated answers does our application provides the updated/right answers 
+     * Comments: This test fails, our application should provide updated values for the question 
+     * 
+     * */
+    
+    @Test
+    public void answersLatestValuesTest() {
+    	Question q = createQuestionEntity();
+        q.setQuestion("question");
+        Mockito.when(repo.save(q)).thenReturn(q);
+        Mockito.when(repo.findByQuestion("question")).thenReturn(Optional.ofNullable(q));
+        service.addQuestion("question","answer1");
+        service.addQuestion("question","answer1"+ "\""+ "answer2"+"\""+"answer3");
+        List<String> answers = service.getAnswers("question");
+        assertEquals(3, answers.size());
+    }
+    
+    /**
+     * Creator: Usman Ashraf
+     * Date: 21.07.2020
+     * Description: Test if the question's best match does not execute
+     * */
+    
+    @Test
+    public void questionBestMatchTest() {
+    	Question q = createQuestionEntity();
+        q.setQuestion("question");
+        Mockito.when(repo.save(q)).thenReturn(q);
+        Mockito.when(repo.findByQuestion("question")).thenReturn(Optional.ofNullable(q));
+        service.addQuestion("question","answer1");
+        List<String> answers = service.getAnswers("Question");
+        assertNotEquals("answer1", answers.get(0));
+    }
+    
+    /**
+     * Creator: Usman Ashraf
+     * Date: 21.07.2020
+     * Description: Test to verify if pass an empty answer while adding question if it prints "the answer to life...." 
+     */
+    @Test
+    public void answersEmptyValueTest() {
+        Question q = createQuestionEntity();
+        q.setQuestion("question");
+        Mockito.when(repo.save(q)).thenReturn(q);
+        Mockito.when(repo.findByQuestion("question")).thenReturn(Optional.ofNullable(q));
+        service.addQuestion("question","");
+        List<String> answers = service.getAnswers("question");
+        assertNotNull(answers);
+        assertEquals("\"" + "the answer to life, universe and everything is 42" + "\"" + " according to" + "\""
+                + "The hitchhikers guide to the Galaxy" + "\"", answers.get(0));
+
+    }
+    
+    
+    /**
+     * Creator: Usman Ashraf
+     * Date: 21.07.2020
+     * Description: Test to verify if pass a null value as answer while adding question if it prints "the answer to life...." 
+     */
+    @Test
+    public void answersNullValueTest() {
+        Question q = createQuestionEntity();
+        q.setQuestion("question");
+        Mockito.when(repo.save(q)).thenReturn(q);
+        Mockito.when(repo.findByQuestion("question")).thenReturn(Optional.ofNullable(q));
+        service.addQuestion("question",null);
+        List<String> answers = service.getAnswers("question");
+        assertNotNull(answers);
+        assertEquals("\"" + "the answer to life, universe and everything is 42" + "\"" + " according to" + "\""
+                + "The hitchhikers guide to the Galaxy" + "\"", answers.get(0));
+    }
+      
+   
 }
--- src/test/java/com/cgm/qanda/util/TestValidationUtil.java
+++ src/test/java/com/cgm/qanda/util/TestValidationUtil.java
@@ -21,7 +21,49 @@ public class TestValidationUtil {
         boolean validate = ValidationUtil.validateLength(input);
         assertEquals(true, validate);
     }
-
+    
+    /**
+     * Creator: Usman Ashraf
+     * Date: 21.07.2020
+     * Description: Test if the input string is more than 255 characters our application should returns false  
+     * Input string length = 370 characters
+     * */
+    
+    @Test
+    public void testValidateLengthBigTestString() {
+        String input = " Optimisation and performance. If you limit the number of characters to 255 this requires only 2 operations on the CPU where as comparison of variable length strings takes many more steps on the CPU, because you have to repeatedly compare across 255 char widths. Programming languages like VBA obscure this a lot because all of the sub-operations are taken care for you.";
+        boolean validate = ValidationUtil.validateLength(input);
+        assertEquals(false, validate);
+    }
+    
+    /**
+     * Creator: Usman Ashraf
+     * Date: 21.07.2020
+     * Description: Test if a double value is provided as a string how our application behaves
+     * */
+    
+    @Test
+    public void testValidateForADoubleValue() {
+    	double input = 1000000000000303030303030.10;
+        boolean validate = ValidationUtil.validateLength( Double.toString(input));
+        assertEquals(true, validate);
+    }
+    
+    /**
+     * Creator: Usman Ashraf
+     * Date: 21.07.2020
+     * Description: Test for an empty string as an input
+     * Comment: This test fails. We need to handle situation where there is an empty string as input
+     * */
+    
+    @Test
+    public void testValidateForEmptyString() {
+    	String input = "";
+        boolean validate = ValidationUtil.validateLength(input);
+        assertEquals(false, validate);
+    }
+    
+  
     @Test
     public void testValidateLengthFailed() {
         String input = null;
@@ -39,6 +81,60 @@ public class TestValidationUtil {
     @Test
     public void testValidateAnswerFormatFailure() {
         String input = "this is wrong input";
+        boolean validate = ValidationUtil.validateAnswerFormat(input);
+        assertEquals(false, validate);
+    }
+    
+    /**
+     * Creator: Usman Ashraf
+     * Date: 21.07.2020
+     * Description: Test for an answer which is more than 255 characters long
+     * Comment: This test fails. Despite having "\" character We did not handle situation where there is string or more than 255 characters
+     * */
+    
+    @Test
+    public void testValidateAnswerFormatLongString() {
+        String input = "this is input " + "\"" +" Optimisation and performance. If you limit the number of characters to 255 this requires only 2 operations on the CPU where as comparison of variable length strings takes many more steps on the CPU, because you have to repeatedly compare across 255 char widths. Programming languages like VBA obscure this a lot because all of the sub-operations are taken care for you.";;
+        boolean validate = ValidationUtil.validateAnswerFormat(input);
+        assertEquals(false, validate);
+    }
+    
+    /**
+     * Creator: Usman Ashraf
+     * Date: 21.07.2020
+     * Description: Test for special characters as an input
+     * */
+    @Test
+    public void testValidateAnswerFormatWithSpecialCharacters() {
+        String input = "&&?%§*@ " + "\"" + "?&/()";
+        boolean validate = ValidationUtil.validateAnswerFormat(input);
+        assertEquals(true, validate);
+    }
+    
+    /**
+     * Creator: Usman Ashraf
+     * Date: 21.07.2020
+     * Description: Test for an null as an input
+     * Comment: This test fails. We need to handle situation where there is a null as input
+     * */
+    
+    @Test
+    public void testValidateAnswerForNullValue() {
+        String input = null;
+        boolean validate = ValidationUtil.validateAnswerFormat(input);
+        assertEquals(false, validate);
+    }
+    
+    /**
+     * Creator: Usman Ashraf
+     * Date: 21.07.2020
+     * Description: Test for an empty string as input
+     * Comment: This test fails. We need to handle situation where there is an empty string as input
+     * */
+    
+    @Test
+    public void testValidateAnswerForEmptyValue() {
+        String input = "\"" ;
         boolean validate = ValidationUtil.validateAnswerFormat(input);
         assertEquals(false, validate);
     }
